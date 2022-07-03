2905f1e626b023b368671480c047672f9b0d0839
Chukwuanieze Eric <47712998+Ericchuk@users.noreply.github.com>
2022-07-03 07:43:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -19,3 +19,60 @@ function openContent(){
 hamburger.addEventListener("click", openContent);
 close.addEventListener("click", openContent)
 
+// asiignment solution 1
+  let arr = [2,3,4,5];
+function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0;
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+    //   sum += arrayOfNumbers[i];
+        console.log(sum += arrayOfNumber)
+    }
+      return arrayOfNumbers.length;
+  }
+
+// sumOfNumbers(arr);
+
+// // assignment solution 2
+// function countEvenNumbers(arrayOfNumbers) {
+    
+//     for(let i = 0; i < arrayOfNumbers.length; i++){
+//         let even = arrayOfNumbers[i] % 2 === 0;
+//         let total = arrayOfNumbers[i];
+//         if(even == true){
+//             console.log(`"even" ${total}`)
+//         }else{
+//             console.log("eee")
+//         }
+        
+//     }
+//     return arrayOfNumbers.length;
+// }
+
+// countEvenNumbers(arr);
+
+function celsiusToFahrenheit(arrayOfNumbers) {
+    let ToFahrenheit = [];
+    
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        let formula = Math.floor((arrayOfNumbers[i] * 1.8) + 32);
+        ToFahrenheit.push(formula);
+        console.log(ToFahrenheit);
+    }
+    return arrayOfNumbers;
+}
+
+celsiusToFahrenheit(arr);
+
+
+let testimonial = document.querySelectorAll(".testimonial aside");
+let testimonialBox = document.querySelector(".testimonial");
+
+let counter = 1;
+let size = testimonial[0].clientWidth;
+console.log(size);
+
+testimonialBox.style.transform = "translateX(`${-size} * ${counter}px`)"
+console.log(`${-size} * ${counter}px`)
+
+// Mouse hover addEventListener
+testimonialBox.addEventListener("mouseleft", () => {})
